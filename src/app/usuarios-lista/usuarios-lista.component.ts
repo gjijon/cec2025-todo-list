03c6a0286b8273e76a146e87ba6a0500f81f9ea0
unknown <gjijon@gmail.com>
2025-07-08 18:26:08 -0500

    lista de usuarios para api

---
+++ src/app/usuarios-lista/usuarios-lista.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-usuarios-lista',
+  imports: [],
+  templateUrl: './usuarios-lista.component.html',
+  styleUrl: './usuarios-lista.component.css'
+})
+export class UsuariosListaComponent {
+
+}
